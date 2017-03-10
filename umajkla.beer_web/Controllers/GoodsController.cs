f936f77b1947e41f0fc23e49f017be6c6461d2b9
Michal Krejčí <michal.krejci@outlook.cz>
2017-03-11 00:35:58 +0100

    Added Goods API, Customers API tweaks

---
+++ umajkla.beer_web/Controllers/GoodsController.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace umajkla.beer.Controllers
+{
+    public class GoodsController : ApiController
+    {
+        // GET api/<controller>/5
+        public HttpResponseMessage Get(string task, int id = 0)
+        {
+            var resp = new HttpResponseMessage();
+            if (task == "list")
+            {
+                resp.StatusCode = HttpStatusCode.OK;
+                resp.Content = new StringContent(JsonConvert.SerializeObject(Models.ShopModel.GetGoodsList()), System.Text.Encoding.UTF8, "application/json");
+            }
+            else if (task == "show")
+            {
+                if (id == 0)
+                {
+                    resp.StatusCode = HttpStatusCode.Ambiguous;
+                    resp.Content = new StringContent("information missing", System.Text.Encoding.UTF8, "text/plain");
+                }
+                else
+                {
+                    resp.StatusCode = HttpStatusCode.OK;
+                    resp.Content = new StringContent(JsonConvert.SerializeObject(Models.ShopModel.GetGoodsDetail(id)), System.Text.Encoding.UTF8, "application/json");
+                }
+            }
+            else
+            {
+                resp.StatusCode = HttpStatusCode.NotImplemented;
+                resp.Content = new StringContent("requested feature could not be understood", System.Text.Encoding.UTF8, "text/plain");
+            }
+            return resp;
+        }
+
+        // POST api/<controller>
+        public HttpResponseMessage Post([FromBody]string details)
+        {
+            var resp = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.Created,
+                Content = new StringContent(JsonConvert.SerializeObject(Models.ShopModel.CreateGoods(details)), System.Text.Encoding.UTF8, "application/json")
+            };
+            return resp;
+        }
+
+        // PUT api/<controller>/5
+        public HttpResponseMessage Put([FromBody]string details)
+        {
+            var resp = new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(JsonConvert.SerializeObject(Models.ShopModel.UpdateGoods(details)), System.Text.Encoding.UTF8, "application/json")
+            };
+            return resp;
+        }
+
+        // DELETE api/<controller>/5
+        public HttpResponseMessage Delete(string task)
+        {
+            HttpResponseMessage resp = new HttpResponseMessage();
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(string.Format("DELETE FROM dbo.goods WHERE id='{0}'", task), connection);
+                connection.Open();
+                if (command.ExecuteNonQuery() == 1)
+                {
+                    resp.StatusCode = HttpStatusCode.OK;
+                    resp.Content = new StringContent("deleted", System.Text.Encoding.UTF8, "text/plain");
+                }
+                else
+                {
+                    resp.StatusCode = HttpStatusCode.NotFound;
+                    resp.Content = new StringContent("transaction could not be found", System.Text.Encoding.UTF8, "text/plain");
+                }
+            }
+            return resp;
+        }
+    }
+}
--- umajkla.beer_web/Models/ShopModel.cs
+++ umajkla.beer_web/Models/ShopModel.cs
@@ -49,6 +49,21 @@ namespace umajkla.beer.Models
                 Notes = reader["notes"].ToString();
             }
         }
+        public class CustomerDetail
+        {
+            public Customer Customer { get; set; }
+            public List<Transaction> Transactions { get; set; }
+            public List<Payment> Payments { get; set; }
+
+            public CustomerDetail(SqlDataReader reader)
+            {
+                Customer = new Customer(reader);
+                Transactions = GetTransactionsForCustomer(Customer.Id);
+                Payments = GetPaymentsForCustomer(Customer.Id);
+            }
+
+        }
+
         public class Payment
         {
             public int Id { get; set; }
@@ -92,6 +107,15 @@ namespace umajkla.beer.Models
                     }
                 }
             }
+
+            public Goods(SqlDataReader reader)
+            {
+                Id = int.Parse(reader["id"].ToString());
+                Name = reader["name"].ToString();
+                Price = int.Parse(reader["price"].ToString());
+                Unit = reader["unit"].ToString();
+                Notes = reader["notes"].ToString();
+            }
         }
         public class Supply
         {
@@ -125,6 +149,155 @@ namespace umajkla.beer.Models
             }
         }
         #endregion
+        #region Customers Methods
+        public static List<CustomerDetail> GetCustomersList()
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                string cmdString = string.Format("SELECT * FROM dbo.customers");
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(cmdString, connection);
+
+                List<CustomerDetail> customers = new List<CustomerDetail>();
+
+                using (SqlDataReader list = command.ExecuteReader())
+                {
+                    while (list.Read())
+                    {
+                        CustomerDetail customer = new CustomerDetail(list);
+                        for (int i = 0; i < customer.Transactions.Count; i++)
+                        {
+                            customer.Transactions[i].Customer = null;
+                        }
+                        for (int i = 0; i < customer.Payments.Count; i++)
+                        {
+                            customer.Payments[i].Customer = null;
+                        }
+                        customers.Add(customer);
+                    }
+                }
+
+                return customers;
+            }
+        }
+
+        public static CustomerDetail GetCustomerDetail(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(string.Format("SELECT * FROM dbo.customers WHERE id='{0}'", id), connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                return new CustomerDetail(reader);
+            }
+        }
+
+        public static CustomerDetail CreateCustomer (string details)
+        {
+            dynamic data = System.Web.Helpers.Json.Decode(details);
+            int customerId;
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                string cmdString = string.Format("INSERT INTO dbo.customers (name, address, phone, email, notes) " +
+                "OUTPUT INSERTED.ID VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')",
+                data.Name, data.Address, data.Phone, data.Email, data.Notes);
+                connection.Open();
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                customerId = (int)command.ExecuteScalar();
+            }
+            return GetCustomerDetail(customerId);
+        }
+
+        public static CustomerDetail UpdateCustomer (string details)
+        {
+            dynamic data = System.Web.Helpers.Json.Decode(details);
+            int customerId;
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                string cmdString = string.Format("UPDATE dbo.customers SET " +
+                    "name='{0}', address='{1}', phone='{2}', email='{3}', notes='{4}', updated='{5}' OUTPUT INSERTED.ID WHERE id='{6}'",
+                    data.Name, data.Address, data.Phone, data.Email, data.Notes, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), data.Id);
+                connection.Open();
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                customerId = (int)command.ExecuteScalar();
+            }
+            return GetCustomerDetail(customerId);
+        }
+        #endregion
+        #region Goods Methods 
+        public static List<Goods> GetGoodsList()
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                string cmdString = string.Format("SELECT * FROM dbo.goods");
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(cmdString, connection);
+
+                List<Goods> goodsList = new List<Goods>();
+
+                using (SqlDataReader list = command.ExecuteReader())
+                {
+                    while (list.Read())
+                    {
+                        Goods goods = new Goods(list);
+                        goodsList.Add(goods);
+                    }
+                }
+
+                return goodsList;
+            }
+        }
+
+        public static Goods GetGoodsDetail(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(string.Format("SELECT * FROM dbo.goods WHERE id='{0}'", id), connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                reader.Read();
+                return new Goods(reader);
+            }
+        }
+
+        public static Goods CreateGoods(string details)
+        {
+            dynamic data = System.Web.Helpers.Json.Decode(details);
+            int goodsId;
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                string cmdString = string.Format("INSERT INTO dbo.goods (name, price, unit, notes) " +
+                "OUTPUT INSERTED.ID VALUES ('{0}', '{1}', '{2}', '{3}')",
+                data.Name, data.Price, data.Unit, data.Notes);
+                connection.Open();
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                goodsId = (int)command.ExecuteScalar();
+            }
+            return GetGoodsDetail(goodsId);
+        }
+
+        public static Goods UpdateGoods(string details)
+        {
+            dynamic data = System.Web.Helpers.Json.Decode(details);
+            int goodsId;
+            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            {
+                string cmdString = string.Format("UPDATE dbo.goods SET " +
+                    "name='{0}', price='{1}', unit='{2}', notes='{3}', updated='{4}' OUTPUT INSERTED.ID WHERE id='{5}'",
+                    data.Name, data.Price, data.Unit, data.Notes, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), data.Id);
+                connection.Open();
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                goodsId = (int)command.ExecuteScalar();
+            }
+            return GetGoodsDetail(goodsId);
+        }
+        #endregion
+        // nutno prepsat update
         #region Payments Methods
         public static List<Payment> GetPaymentsList()
         {
@@ -215,76 +388,7 @@ namespace umajkla.beer.Models
             return GetTransactionDetail(paymentId);
         }
         #endregion
-        #region Customers Methods
-        public static List<Customer> GetCustomersList()
-        {
-            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
-            {
-                string cmdString = string.Format("SELECT * FROM dbo.customers");
-
-                connection.Open();
-
-                SqlCommand command = new SqlCommand(cmdString, connection);
-
-                List<Customer> customers = new List<Customer>();
-
-                using (SqlDataReader list = command.ExecuteReader())
-                {
-                    while (list.Read())
-                    {
-                        Customer customer = new Customer(list);
-                        customers.Add(customer);
-                    }
-                }
-
-                return customers;
-            }
-        }
-
-        public static Customer GetCustomerDetail(int id)
-        {
-            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
-            {
-                SqlCommand command = new SqlCommand(string.Format("SELECT * FROM dbo.customers WHERE id='{0}'", id), connection);
-                connection.Open();
-                SqlDataReader reader = command.ExecuteReader();
-                reader.Read();
-                return new Customer(reader);
-            }
-        }
-
-        public static Customer CreateCustomer (string details)
-        {
-            dynamic data = System.Web.Helpers.Json.Decode(details);
-            int customerId;
-            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
-            {
-                string cmdString = string.Format("INSERT INTO dbo.customers (name, address, phone, email, notes) " +
-                "OUTPUT INSERTED.ID VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')",
-                data.Name, data.Address, data.Phone, data.Email, data.Notes);
-                connection.Open();
-                SqlCommand command = new SqlCommand(cmdString, connection);
-                customerId = (int)command.ExecuteScalar();
-            }
-            return GetCustomerDetail(customerId);
-        }
-
-        public static Customer UpdateCustomer (string details)
-        {
-            dynamic data = System.Web.Helpers.Json.Decode(details);
-            int customerId;
-            using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
-            {
-                string cmdString = string.Format("UPDATE dbo.customers SET " +
-                    "name='{0}', address='{1}', phone='{2}', email='{3}', notes='{4}', updated='{5}' OUTPUT INSERTED.ID WHERE id='{6}'",
-                    data.Name, data.Address, data.Phone, data.Email, data.Notes, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), data.Id);
-                connection.Open();
-                SqlCommand command = new SqlCommand(cmdString, connection);
-                customerId = (int)command.ExecuteScalar();
-            }
-            return GetCustomerDetail(customerId);
-        }
-        #endregion
+        // nutno prepsat update
         #region Transaction Methods
         public static List<Transaction> GetTransactionsList()
         {
@@ -315,7 +419,7 @@ namespace umajkla.beer.Models
         {
             using (SqlConnection connection = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
             {
-                string cmdString = string.Format("SELECT * FROM dbo.payments WHERE customerId='{0}'", customerId);
+                string cmdString = string.Format("SELECT * FROM dbo.transactions WHERE customerId='{0}'", customerId);
                 SqlCommand command = new SqlCommand(cmdString, connection);
                 connection.Open();
                 List<Transaction> transactions = new List<Transaction>();
@@ -375,5 +479,6 @@ namespace umajkla.beer.Models
             return GetTransactionDetail(transactionId);
         }
         #endregion
+
     }
 }
